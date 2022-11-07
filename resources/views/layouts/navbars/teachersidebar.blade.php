93b33b36632b8f28cedfc4c0c17f505bd93b84d2
Hanani <nurulhnani@gmail.com>
2022-11-08 03:32:43 +0800

    edit sidebar href

--- resources/views/layouts/navbars/teachersidebar.blade.php
+++ resources/views/layouts/navbars/teachersidebar.blade.php
@@ -100,7 +100,7 @@
                     <div class="collapse show" id="navbar-examples">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('profile.edit') }}">
+                                <a class="nav-link" href="">
                                     {{ __('Student Profile') }}
                                 </a>
                             </li>
