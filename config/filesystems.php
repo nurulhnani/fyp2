735e6dfe4392390f2725c0b3307b825f9c57b766
Hanani <nurulhnani@gmail.com>
2023-01-11 06:37:58 +0800

    filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,10 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            // 'root' => storage_path('app/public'),
+            // 'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
             'throw' => false,
         ],
