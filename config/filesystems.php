57f87a9edc4348e83b184c23be622c9c3c1251f7
nuramirah <nuramiramohamed20@gmail.com>
2023-01-13 01:43:26 +0800

    try img path

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,8 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        // public_path('storage') => storage_path('app/public'),
+        base_path('/public_html/storage') => storage_path('app/public'),
     ],
 
 ];
