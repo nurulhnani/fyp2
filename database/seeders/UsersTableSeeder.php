2fd81f948b977ce7e3d093c09c229c5a5cb794e9
Nur Amirah <nuramiramohamed20@gmail.com>
2023-01-14 12:25:23 +0800

    seeder user student teacher image path null

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -54,8 +54,9 @@ class UsersTableSeeder extends Seeder
             ],
             [
                 'name'=>'Sabarina binti Kamal',
-                'image_path' => 'Sabarina binti Kamal.png',
+                'image_path' => null,
                 'nric_mykid'=>'890716-01-2236',
+                'email'=>'sabarina@gmail.com',
                 'type'=>1,
                 'email_verified_at' => now(),
                 'first_login' => 1,
@@ -66,8 +67,9 @@ class UsersTableSeeder extends Seeder
              ],
              [
                 'name'=>'Mohd Ridwan bin Daud',
-                'image_path' => 'Mohd Ridwan bin Daud.png',
+                'image_path' => null,
                 'nric_mykid'=>'890721-14-2231',
+                'email'=>'ridwan@gmail.com',
                 'type'=>1,
                 'email_verified_at' => now(),
                 'first_login' => 1,
@@ -78,7 +80,7 @@ class UsersTableSeeder extends Seeder
              ],
             [
                 'name'=>'Mohd Hafiz bin Kamal',
-                'image_path' => 'Mohd Hafiz bin Kamal.png',
+                'image_path' => null,
                 'nric_mykid'=>'MH12123',
                 'type'=>2,
                 'first_login' => 1,
@@ -89,7 +91,7 @@ class UsersTableSeeder extends Seeder
                  'updated_at' => now()
             ],[
                 'name'=>'Nurul Hanani',
-                'image_path' => 'Nurul Hanani.png',
+                'image_path' => null,
                 'nric_mykid'=>'000729141107',
                 'type'=>2,
                 'first_login' => 1,
@@ -100,9 +102,20 @@ class UsersTableSeeder extends Seeder
                  'updated_at' => now()
             ],[
                 'name'=>'Nur Amirah',
-                'image_path' => 'Nurul Amirah.png',
+                'image_path' => null,
                 'nric_mykid'=>'000320011148',
                 'email'=>'nuramiramohamed20@gmail.com',
+                'type'=>2,
+                'first_login' => 0,
+                'logincount' => 0,
+                'email_verified_at' => now(),
+                 'password' => Hash::make('secret'),
+                 'created_at' => now(),
+                 'updated_at' => now()
+            ],[
+                'name'=>'Aisyah Sofea',
+                'image_path' => null,
+                'nric_mykid'=>'000505111103',
                 'type'=>2,
                 'first_login' => 0,
                 'logincount' => 0,
